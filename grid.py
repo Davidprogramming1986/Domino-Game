9359561bf035c4277620bd09c06ffde79af17a1f
Davidprogramming1986 <davidholder@Davids-iMac.local>
2017-08-27 19:04:59 +0100

    rendering full grid

--- grid.py
+++ grid.py
@@ -6,8 +6,18 @@ pygame.init()
 
 class Grid():
 
-    #def __init__(self):
-
+    def __init__(self):
+        self.grid = []
 
     def render_grid(self):
-        pygame.draw.rect(defs.display, colors.BLACK, (defs.width * defs.x_grid_gap, defs.height * defs.y_grid_gap, defs.grid_square_len * defs.x_grid_squares, defs.grid_square_len * defs.y_grid_squares), 1)
+        x = defs.width * defs.x_grid_gap
+        y = defs.height * defs.y_grid_gap
+        width = defs.grid_square_len * defs.x_grid_squares
+        height = defs.grid_square_len * defs.y_grid_squares
+        pygame.draw.rect(defs.display, colors.BLACK, (x, y, width, height), 1)
+
+        for gap in range(defs.x_grid_squares):
+            pygame.draw.line(defs.display, colors.BLACK, (x + (gap * defs.grid_square_len), y), (x + (gap * defs.grid_square_len), y + height))
+
+        for gap in range(defs.y_grid_squares):
+            pygame.draw.line(defs.display, colors.BLACK, (x, y + (gap * defs.grid_square_len)), (x + width, y + (gap * defs.grid_square_len)))
